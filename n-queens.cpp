7fb4a5de78471b7edb2918890d6c14c7d8c2919e
bpime <b.pimed@gmail.com>
2021-10-29 09:08:34 -0500

    parallelization

--- n-queens.cpp
+++ n-queens.cpp
@@ -2,8 +2,8 @@
 #include <deque>
 
 int total = 0;
-bool isEatable(int row, int column, std::deque<int> &queens){
-    for(int i = 0; i < queens.size(); i++){
+bool isEatable(int row, int column, std::deque<int> &queens) {
+    for(int i = 0; i < queens.size(); i++) {
         if(row == i) return true;
         if(column == queens[i]) return true;
         if(i - queens[i] == row - column) return true;
@@ -13,28 +13,31 @@ bool isEatable(int row, int column, std::deque<int> &queens){
     return false;
 }
 
-int solve(int n, int row, std::deque<int> &queens){
+int solve(int n, int row, std::deque<int> &queens) {
     if(queens.size() == n) {
         /*for(auto i: queens)
             std::cout << i <<" ";
         std::cout << std::endl;*/
-        total ++;
+        total++;
         return 1;
     }
-    for(int i = 0; i < n; i++){
-        if(!isEatable(row,i,queens)){
+
+    #pragma omp parallel
+    #pragma omp task shared(queens)
+    for(int i = 0; i < n; i++) { 
+        if(!isEatable(row,i,queens)) {
             if(solve(n, row + 1, queens)) {
                 queens.pop_back();//return here to end in the first solution
                 continue;
             }
             else queens.pop_back();
         }
+        #pragma omp taskwait
     }    
     return 0;
 }
 
-int main(int argc, char **argv) {
-    
+int main(int argc, char **argv) {   
     std::string n;
     if (argc > 1)
         n = argv[1];
